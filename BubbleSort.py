a5bd5f2db2072b43de65cca4642938d94d6667b7
shiningskyy <40699129+shiningskyy@users.noreply.github.com>
2021-12-26 16:57:15 +0100

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -2,7 +2,8 @@
 import random
 import matplotlib.pyplot as plt
 
-#filling list with random numbers
+
+# filling list with random numbers
 def fillList(sampleList, N, Y):
     assert type(sampleList) == list
     for i in range(0, N):
@@ -10,23 +11,27 @@ def fillList(sampleList, N, Y):
         sampleList.append(i)
     return sampleList
 
+
 def bubbleSort(sampleList):
     assert type(sampleList) == list
     n = len(sampleList)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if sampleList[j] > sampleList[j+1]:
-                sampleList[j], sampleList[j+1] = sampleList[j+1], sampleList[j]
-            updateGraph(sampleList)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if sampleList[j] > sampleList[j + 1]:
+                sampleList[j], sampleList[j + 1] = sampleList[j + 1], sampleList[j]
+            updateGraph(sampleList, j+1)
     return sampleList
 
-def updateGraph(sampleList):
-    plt.bar(range(0, N), sampleList)
+
+def updateGraph(sampleList, index):
+    bars = plt.bar(range(0, N), sampleList)
+    bars[index].set_color('r')
     plt.draw()
     plt.pause(0.1)
     plt.clf()
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     N = int(input("Number of N numbers to sort => "))
     Y = int(input("Max value => "))
     sampleList = []
